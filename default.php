6c61ea64c83ab2bcdfe218848572860cc66a5c15
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-26 14:35:58 +0200

    php session variable

--- default.php
+++ default.php
@@ -2087,3 +2087,43 @@
 // A session is a way to store information (in variables) to be used across multiple pages.
 // Unlike a cookie, the information is not stored on the users computer.
 ?>
+
+// What is a PHP Session?
+<?php
+// When you work with an application, you open it, do some changes, and then you close it.
+// This is much like a Session. The computer knows who you are. It knows when you start the application and when you end.
+// But on the internet there is one problem: the web server does not know who you are or what you do, because the HTTP address doesn't maintain state.
+
+// Session variables solve this problem by storing user information to be used across multiple pages (e.g. username, favorite color, etc). By default, session variables last until the user closes the browser.
+?>
+
+// Start a PHP Session
+<?php
+//A session is started with the session_start() function.
+// Session variables are set with the PHP global variable: $_SESSION.
+// Now, let's create a new page called "demo_session1.php". In this page, we start a new PHP session and set some session variables:
+?>
+
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
+
+</body>
+</html>
+
+// result 
+<?php
+// Session variables are set.
+?>
+
