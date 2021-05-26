777c9e37b690ad5361cdbe6d8530871548378fdb
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-26 15:19:56 +0200

    Destroy the php session variables

--- default.php
+++ default.php
@@ -2207,3 +2207,26 @@ print_r($_SESSION);
 
 // result 
 // Array ( [favcolor] => yellow )
+
+// Destroy a PHP Session
+// To remove all global session variables and destroy the session, use session_unset() and session_destroy():
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// remove all session variables
+session_unset();
+
+// destroy the session
+session_destroy();
+
+echo "All session variables are now removed, and the session is destroyed."
+?>
+
+</body>
+</html>
