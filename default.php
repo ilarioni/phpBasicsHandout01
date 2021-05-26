3671760dc33f3038acc4e9e57a61c06780a47cb6
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-26 14:28:55 +0200

    php sessions

---
+++ default.php
@@ -0,0 +1,5 @@
+// php Sessions
+<?php 
+// A session is a way to store information (in variables) to be used across multiple pages.
+// Unlike a cookie, the information is not stored on the users computer.
+?>
