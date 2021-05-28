89650a4acebf2f74eb5a1e54735bc5f7300d7c89
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-28 16:40:40 +0200

    try catch statement

--- default.php
+++ default.php
@@ -2654,3 +2654,25 @@ Fatal error: Uncaught Exception: Division by zero in C:\webfolder\test.php:4
 Stack trace: #0 C:\webfolder\test.php(9):
 divide(5, 0) #1 {main} thrown in C:\webfolder\test.php on line 4
 
+
+The try...catch Statement
+
+To avoid the error from the example above, we can use the try...catch statement to catch exceptions and continue the process.
+
+<?php
+function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero");
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide(5, 0);
+} catch(Exception $e) {
+  echo "Unable to divide.";
+}
+?>
+
+// result  ---  Unable to divide.
+
