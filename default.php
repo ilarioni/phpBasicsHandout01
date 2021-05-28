30a1fa1c1eb8a5bfcfa5d36a55ec6186ee025fd3
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-28 17:04:25 +0200

    thrown exception

--- default.php
+++ default.php
@@ -2727,3 +2727,29 @@ try {
 
 // result --- Process complete.
 
+
+Output information about an exception that was thrown:
+
+<?php
+function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero", 1);
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide(5, 0);
+} catch(Exception $ex) {
+  $code = $ex->getCode();
+  $message = $ex->getMessage();
+  $file = $ex->getFile();
+  $line = $ex->getLine();
+  echo "Exception thrown in $file on line $line: [Code $code]
+  $message";
+}
+?>
+
+// result --- Exception thrown in /home/TwT0ln/prog.php on line 8: [Code 1] Division by zero
+
+
