110205db5b28d90d82ae1816b48434d040f41f35
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-28 16:24:39 +0200

    looping through the values

--- default.php
+++ default.php
@@ -2589,3 +2589,35 @@ echo $arr["Joe"];
 // result 353743
 
 PHP - Looping Through the Values
+You can also loop through the values with a foreach() loop:
+
+<?php
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+
+$obj = json_decode($jsonobj);
+
+foreach($obj as $key => $value) {
+  echo $key . " => " . $value . "<br>";
+}
+?>
+
+// result  --- 
+Peter => 35
+Ben => 37
+Joe => 43
+
+This example shows how to loop through the values of a PHP associative array:
+<?php
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+
+$arr = json_decode($jsonobj, true);
+
+foreach($arr as $key => $value) {
+  echo $key . " => " . $value . "<br>";
+}
+?>
+
+// result ---
+Peter => 35
+Ben => 37
+Joe => 43
