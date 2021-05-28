c56ac104e1ea2e046d57690e12cb3d51fa3a0788
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-28 16:34:59 +0200

    php exceptions

--- default.php
+++ default.php
@@ -2621,3 +2621,36 @@ foreach($arr as $key => $value) {
 Peter => 35
 Ben => 37
 Joe => 43
+
+
+////// 
+PHP Exceptions
+
+An exception is an object that describes an error or unexpected behaviour of a PHP script.
+Exceptions are thrown by many PHP functions and classes.
+User defined functions and classes can also throw exceptions.
+Exceptions are a good way to stop a function when it comes across data that it cannot use.
+
+
+Throwing an Exception
+
+The throw statement allows a user defined function or method to throw an exception. When an exception is thrown, the code following it will not be executed.
+If an exception is not caught, a fatal error will occur with an "Uncaught Exception" message.
+Lets try to throw an exception without catching it:
+
+<?php
+function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero");
+  }
+  return $dividend / $divisor;
+}
+
+echo divide(5, 0);
+?>
+
+// result  --- 
+Fatal error: Uncaught Exception: Division by zero in C:\webfolder\test.php:4
+Stack trace: #0 C:\webfolder\test.php(9):
+divide(5, 0) #1 {main} thrown in C:\webfolder\test.php on line 4
+
