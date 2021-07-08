3e29b8db4520a686aac89a9ec216d5a377d96607
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-08 17:01:52 +0200

    PHP OOP

--- default.php
+++ default.php
@@ -2754,3 +2754,27 @@ try {
 
 
 PHP - What is OOP?
+
+From PHP5, you can also write PHP code in an object-oriented style.
+Object-Oriented programming is faster and easier to execute.
+
+OOP stands for Object-Oriented Programming.
+
+Procedural programming is about writing procedures or functions that perform operations on the data, while object-oriented programming is about creating objects that contain both data and functions.
+
+Object-oriented programming has several advantages over procedural programming:
+
+
+> OOP is faster and easier to execute
+> OOP provides a clear structure for the programs
+> OOP helps to keep the PHP code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
+> OOP makes it possible to create full reusable applications with less code and shorter development time
+
+// PHP - What are Classes and Objects?
+Classes and objects are the two main aspects of object-oriented programming.
+
+So, a class is a template for objects, and an object is an instance of a class.
+When the individual objects are created, they inherit all the properties and behaviors from the class, but each object will have different values for the properties.
+
+
+
