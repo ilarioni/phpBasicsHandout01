1a3ea2f297cbae053f613db31c160f44746cef77
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-09 15:32:20 +0200

    PHP class and objects
    $this keyword
    class contains several objects and objects inherit all properties and methods from class

--- default.php
+++ default.php
@@ -353,6 +353,7 @@
         Hello World!
     -->
 
+
     <h3>PHP Function Arguments</h3>
     <p>Information can be passed to functions through arguments. An argument is just like a variable.</p>
     <p>Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.</p>
@@ -2778,5 +2779,188 @@ When the individual objects are created, they inherit all the properties and beh
 
 
 
-PHP OOP - Classes and Objects
+<h3> PHP OOP - Classes and Objects </h3> 
+
+<!--fdfdf-->
+<h1> </h1>
+<?php  
+    A is a template for objects, and an object is an instance of class.
+?>
+
+<?php
+OOP Case
+
+Let s assume we have a class named Fruit. A Fruit can have properties like name, color, weight, etc. We can define variables like $name, $color, and $weight to hold the values of these properties.
+
+When the individual objects (apple, banana, etc.) are created, they inherit all the properties and behaviors from the class, but each object will have different values for the properties.
+
+?>
+
+<h3> Define a Class </h3>
+<?php
+A class is defined by using the class keyword, followed by the name of the class and a pair of curly braces ({}). All its properties and methods go inside the braces:
+
+?>
+
+        example: 
+        <?php
+        class Fruit {
+        // code goes here...
+        }
+        ?>
+
+
+        Below we declare a class named Fruit consisting of two properties ($name and $color) and two methods set_name() and get_name() for setting and getting the $name property:
+
+
+        <?php
+        class Fruit {
+            // Properties
+            public $name;
+            public $color;
+
+            // Methods
+            function set_name($name) {
+                $this->name = $name;
+            }
+            function get_name() {
+                return $this->name;
+            }
+        }
+        ?>
+
+        Note: In a class, variables are called properties and functions are called methods!
+
+        Define Objects
+
+        Classes are nothing without objects! We can create multiple objects from a class. Each object has all the properties and methods defined in the class, but they will have different property values.
+
+        Objects of a class is created using the new keyword.
+        In the example below, $apple and $banana are instances of the class Fruit:
+
+        <?php
+        class Fruit {
+        // Properties
+            public $name;
+            public $color;
+
+            // Methods
+            function set_name($name) {
+                $this->name = $name;
+            }
+            function get_name() {
+                return $this->name;
+            }
+        }
+
+        $apple = new Fruit();
+        $banana = new Fruit();
+        $apple->set_name('Apple');
+        $banana->set_name('Banana');
+
+        echo $apple->get_name();
+        echo "<br>";
+        echo $banana->get_name();
+        ?>
+
+        In the example below, we add two more methods to class Fruit, for setting and getting the $color property:
+
+            <?php
+            class Fruit {
+                // Properties
+                public $name;
+                public $color;
+
+                // Methods
+                function set_name($name) {
+                    $this->name = $name;
+                }
+                function get_name() {
+                    return $this->name;
+                }
+                function set_color($color) {
+                    $this->color = $color;
+                }
+                function get_color() {
+                    return $this->color;
+                }
+            }
+
+            $apple = new Fruit();
+            $apple->set_name('Apple');
+            $apple->set_color('Red');
+            echo "Name: " . $apple->get_name();
+            echo "<br>";
+            echo "Color: " .  $apple->get_color();
+            ?>
+
+        
+
+        PHP - The $this Keyword
+
+        The $this keyword refers to the current object, and is only available inside methods.
+        Look at the following example:
+
+        <?php
+        class Fruit {
+            public $name;
+        }
+        $apple = new Fruit();
+        ?>
+
+        So, where can we change the value of the $name property? There are two ways:
+
+        1. Inside the class (by adding a set_name() method and use $this):
+
+        <?php
+        class Fruit {
+            public $name;
+            function set_name($name) {
+                $this->name = $name;
+            }
+        }
+        $apple = new Fruit();
+        $apple->set_name("Apple");
+        ?>
+
+        2. Outside the class (by directly changing the property value):
+
+        <?php
+        class Fruit {
+            public $name;
+        }
+        $apple = new Fruit();
+        $apple->name = "Apple";
+        ?>
+
+        PHP - instanceof
+
+        You can use the instanceof keyword to check if an object belongs to a specific class:
+
+        <?php
+        class Fruit {
+            // Properties
+            public $name;
+            public $color;
+
+            // Methods
+            function set_name($name) {
+                $this->name = $name;
+            }
+            function get_name() {
+                return $this->name;
+            }
+        }
+
+        $apple = new Fruit();
+        var_dump($apple instanceof Fruit);
+        ?>
+
+
+    PHP OOP - Constructor
+
+
+
+
+
 
