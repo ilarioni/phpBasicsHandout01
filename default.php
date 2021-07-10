8bd57af25471c77acf41c7d387fb3870a82d98c8
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-10 19:46:59 +0200

    PHP __construct

--- default.php
+++ default.php
@@ -2959,6 +2959,65 @@ A class is defined by using the class keyword, followed by the name of the class
 
     PHP OOP - Constructor
 
+    A constructor allows you to initialize an object's properties upon creation of the object.
+    If you create a __construct() function, PHP will automatically call this function when you create an object from a class.
+
+    Notice that the construct function starts with two underscores (__)!
+
+
+    We see in the example below, that using a constructor saves us from calling the set_name() method which reduces the amount of code:
+
+    <?php
+    class Fruit {
+        public $name;
+        public $color;
+
+        function __construct($name) {
+            $this->name = $name; 
+        }
+        function get_name() {
+            return $this->name;
+        }
+    }
+
+    $apple = new Fruit("Apple");
+    echo $apple->get_name();
+    ?>
+
+    result 
+    Apple 
+
+    Another example:
+
+    <?php
+    class Fruit {
+        public $name;
+        public $color;
+
+        function __construct($name, $color) {
+            $this->name = $name;
+            $this->color = $color;
+        }
+        function get_name() {
+            return $this->name;
+        }
+        function get_color() {
+            return $this->color;
+        }
+    }
+
+    $apple = new Fruit("Apple", "red");
+    echo $apple->get_name();
+    echo "<br>";
+    echo $apple->get_color();
+    ?>
+
+    // result 
+    Apple
+    red
+
+
+    PHP OOP - Destructor
 
 
 
