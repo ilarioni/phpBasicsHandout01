bb4d128c285abd5439d6b43ecec894f11783ae1a
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-12 16:17:12 +0200

    inheritance in PHP
    class inheritance
    as well as properties and methods

--- default.php
+++ default.php
@@ -3126,5 +3126,53 @@ A class is defined by using the class keyword, followed by the name of the class
 
     PHP OOP - Inheritance
     
+    Inheritance in OOP = When a class derives from another class.
+
+    The child class will inherit all the public and protected properties and methods from the parent class. In addition, it can have its own properties and methods.
+    An inherited class is defined by using the extends keyword.
+
+    Let's look at an example:
+
+    <?php
+    class Fruit {
+        public $name;
+        public $color;
+        public function __construct($name, $color) {
+            $this->name = $name;
+            $this->color = $color; 
+        }
+        public function intro() {
+            echo "The fruit is {$this->name} and the color is {$this->color}."; 
+        }
+    }
+
+    // Strawberry is inherited from Fruit
+    class Strawberry extends Fruit {
+        public function message() {
+            echo "Am I a fruit or a berry? "; 
+        }
+    }
+
+    $strawberry = new Strawberry("Strawberry", "red");
+    $strawberry->message();
+    $strawberry->intro();
+    ?>
+
+    The Strawberry class is inherited from the Fruit class.
+
+    This means that the Strawberry class can use the public $name and $color properties as well as the public __construct() and intro() methods from the Fruit class because of inheritance.
+
+
+    The Strawberry class also has its own method: message().
+
+    <h1> PHP - Inheritance and the Protected Access Modifier </h1>
+
+    In the previous chapter we learned that protected properties or methods can be accessed within the class and by classes derived from that class. What does that mean?
+    Let's look at an example:
+
+    
+
+
+
 
 
