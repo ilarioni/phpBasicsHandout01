8a91ccb543178418c1cc603a68e5a16cf61f8e11
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-12 16:28:07 +0200

    inheritance and protected access modifiers
    PHP inheritance - public, protected

--- default.php
+++ default.php
@@ -3170,7 +3170,64 @@ A class is defined by using the class keyword, followed by the name of the class
     In the previous chapter we learned that protected properties or methods can be accessed within the class and by classes derived from that class. What does that mean?
     Let's look at an example:
 
+    <?php
+    class Fruit {
+        public $name;
+        public $color;
+        public function __construct($name, $color) {
+            $this->name = $name;
+            $this->color = $color; 
+        }
+        protected function intro() {
+            echo "The fruit is {$this->name} and the color is {$this->color}."; 
+        }
+    }
+
+    class Strawberry extends Fruit {
+        public function message() {
+            echo "Am I a fruit or a berry? "; 
+        }
+    }
+
+    // Try to call all three methods from outside class
+    $strawberry = new Strawberry("Strawberry", "red");  // OK. __construct() is public
+    $strawberry->message(); // OK. message() is public
+    $strawberry->intro(); // ERROR. intro() is protected
+    ?>
+
+    In the example above we see that if we try to call a protected method (intro()) from outside the class, we will receive an error. public methods will work fine!
+
+    Let's look at another example:
+
+    <?php
+    class Fruit {
+        public $name;
+        public $color;
+        public function __construct($name, $color) {
+            $this->name = $name;
+            $this->color = $color; 
+        }
+        protected function intro() {
+            echo "The fruit is {$this->name} and the color is {$this->color}."; 
+        }
+    }
+
+    class Strawberry extends Fruit {
+        public function message() {
+            echo "Am I a fruit or a berry? ";
+            // Call protected function from within derived class - OK 
+            $this -> intro();
+        }
+    }
+    $strawberry = new Strawberry("Strawberry", "red");  // OK. __construct() is public
+    $strawberry->message(); // OK. message() is public and it calls intro() (which is protected) from within the derived class
+    ?>
+
+    In the example above we see that all works fine! It is because we call the protected method (intro()) from inside the derived class.
+
     
+
+
 
 
 
