2c9a30a7b593dc8988e47947f1337185fa279a48
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-18 21:37:56 +0200

    example explained

--- default.php
+++ default.php
@@ -3670,4 +3670,9 @@ A class is defined by using the class keyword, followed by the name of the class
     result is following:
     OOP is fun!
 
+    Example Explained
+    Here, we declare one trait: message1. Then, we create a class: Welcome. The class uses the trait, and all the methods in the trait will be available in the class.
+
+    If other classes need to use the msg1() function, simply use the message1 trait in those classes. This reduces code duplication, because there is no need to redeclare the same method over and over again.
+
     
