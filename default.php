0daaf49e2a302bde2588d701f7efcf27f7b5d9c2
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-18 22:20:37 +0200

    commit delete

--- default.php
+++ default.php
@@ -3641,42 +3641,4 @@ A class is defined by using the class keyword, followed by the name of the class
     }
     ?>
 
-    PHP - Using Multiple Traits
-
-    Let's look at another example:
-
-    <?php
-    trait message1 {
-        public function msg1() {
-            echo "OOP is fun! ";
-        }
-    }
-
-    trait message2 {
-        public function msg2() {
-            echo "OOP reduces code duplication!";
-        }
-    }
-
-    class Welcome {
-        use message1;
-    }
-
-    class Welcome2 {
-        use message1, message2;
-    }
-
-    $obj = new Welcome();
-    $obj->msg1();
-    echo "<br>";
-
-    $obj2 = new Welcome2();
-    $obj2->msg1();
-    $obj2->msg2();
-    ?>
-
-    result:
-    OOP is fun!
-    OOP is fun! OOP reduces code duplication!
-
-
+    
