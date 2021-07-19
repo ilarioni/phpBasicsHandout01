3d27df74f67eb3662a6c50b740b88e551a93d282
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-19 20:52:07 +0200

    PHP multiple traits
    several trait inheriting

--- default.php
+++ default.php
@@ -3672,5 +3672,42 @@ A class is defined by using the class keyword, followed by the name of the class
     Here, we declare one trait: message1. Then, we create a class: Welcome. The class uses the trait, and all the methods in the trait will be available in the class.
     If other classes need to use the msg1() function, simply use the message1 trait in those classes. This reduces code duplication, because there is no need to redeclare the same method over and over again.
 
-    
+    PHP - Using Multiple Traits
 
+    Let's look at another example:
+    <?php
+    trait message1 {
+        public function msg1() {
+            echo "OOP is fun! ";
+        }
+    }
+
+    trait message2 {
+        public function msg2() {
+            echo "OOP reduces code duplication!";
+        }
+    }
+
+    class Welcome {
+        use message1;
+    }
+
+    class Welcome2 {
+        use message1, message2;
+    }
+
+    $obj = new Welcome();
+    $obj->msg1();
+    echo "<br>";
+
+    $obj2 = new Welcome2();
+    $obj2->msg1();
+    $obj2->msg2();
+    ?>
+
+    result is following:
+    OOP is fun!
+    OOP is fun! OOP reduces code duplication!
+
+    Here, we declare two traits: message1 and message2. Then, we create two classes: Welcome and Welcome2. The first class (Welcome) uses the message1 trait, and the second class (Welcome2) uses both message1 and message2 traits (multiple traits are separated by comma).
+    
