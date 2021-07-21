94a3f9154f04ef7239fda3eb8ec9231fe37ed3ec
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-21 10:22:45 +0200

    static property
    accessing from child to parent
    calling directly

--- default.php
+++ default.php
@@ -3836,4 +3836,49 @@ A class is defined by using the class keyword, followed by the name of the class
     Example Explained
     Here, we declare a static property: $value. Then, we echo the value of the static property by using the class name, double colon (::), and the property name (without creating a class first).
 
-    
+    More on Static Properties
+    A class can have both static and non-static properties. A static property can be accessed from a method in the same class using the self keyword and double colon (::):
+
+    <?php
+    class pi {
+        public static $value=3.14159;
+        public function staticValue() {
+            return self::$value;
+        }
+    }
+
+    // Get static property
+    $pi = new pi();
+    echo $pi->staticValue();
+    ?>
+
+    result:
+    3.14159
+
+    To call a static property from a child class, use the parent keyword inside the child class:
+    <?php
+    class pi {
+        public static $value=3.14159;
+    }
+
+    class x extends pi {
+        public function xStatic() {
+            return parent::$value;
+        }
+    }
+
+    // Get value of static property directly via child class
+    echo x::$value;
+    echo "<br>";
+
+    // or get value of static property via xStatic() method
+    $x = new x();
+    echo $x->xStatic();
+    ?>
+
+    returning:
+    3.14159
+    3.14159
+
+
+    <h1> PHP Namespaces </h1>
