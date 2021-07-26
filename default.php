930096029d1041d0a07c9a7067b8492e7d9949c2
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-26 13:31:01 +0200

    php xml

--- default.php
+++ default.php
@@ -4393,11 +4393,39 @@ A class is defined by using the class keyword, followed by the name of the class
 
     The PHP script loads an XML document, "cd_catalog.xml", runs a query against the XML file, and returns the result as HTML:
 
+    <?php
+    $q=$_GET["q"];
+
+    $xmlDoc = new DOMDocument();
+    $xmlDoc->load("cd_catalog.xml");
+
+    $x=$xmlDoc->getElementsByTagName('ARTIST');
+
+    for ($i=0; $i<=$x->length-1; $i++) {
+    //Process only element nodes
+        if ($x->item($i)->nodeType==1) {
+            if ($x->item($i)->childNodes->item(0)->nodeValue == $q) {
+            $y=($x->item($i)->parentNode);
+            }
+        }
+    }
+
+    $cd=($y->childNodes);
+
+    for ($i=0;$i<$cd->length;$i++) {
+    //Process only element nodes
+        if ($cd->item($i)->nodeType==1) {
+            echo("<b>" . $cd->item($i)->nodeName . ":</b> ");
+            echo($cd->item($i)->childNodes->item(0)->nodeValue);
+            echo("<br>");
+        }
+    }
+    ?>
+
+    When the CD query is sent from the JavaScript to the PHP page, the following happens:
+
+        PHP creates an XML DOM object
+        Find all <artist> elements that matches the name sent from the JavaScript
+        Output the album information (send to the "txtHint" placeholder)
+
     
-
-
-
-    
-    
-
-
