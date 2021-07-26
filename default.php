bfcb685b3a6db8506f6b2a3c30114d6b7d9b79d9
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-26 12:52:58 +0200

    get data
    from MySQL database

--- default.php
+++ default.php
@@ -4253,3 +4253,45 @@ A class is defined by using the class keyword, followed by the name of the class
     AJAX Database - AJAX and MySQL
     AJAX can be used for interactive communication with a database.
 
+    The following example will demonstrate how a web page can fetch information from a database with AJAX:
+    
+    In the example above, when a user selects a person in the dropdown list above, a function called "showUser()" is executed.
+    The function is triggered by the onchange event.
+    Here is the HTML code:
+
+    <script>
+    function showUser(str) {
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("txtHint").innerHTML = this.responseText;
+        }
+        };
+        xmlhttp.open("GET","getuser.php?q="+str,true);
+        xmlhttp.send();
+    }
+    }
+    </script>
+    </head>
+    <body>
+
+    <form>
+    <select name="users" onchange="showUser(this.value)">
+    <option value="">Select a person:</option>
+    <option value="1">Peter Griffin</option>
+    <option value="2">Lois Griffin</option>
+    <option value="3">Joseph Swanson</option>
+    <option value="4">Glenn Quagmire</option>
+    </select>
+    </form>
+    <br>
+    <div id="txtHint"><b>Person info will be listed here...</b></div>
+
+    
+
+
+    
