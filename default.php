921064813842b277f6b198260539903186dfb13b
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-26 13:50:16 +0200

    back - live search explained

--- default.php
+++ default.php
@@ -4479,4 +4479,62 @@ A class is defined by using the class keyword, followed by the name of the class
         Send the request off to a file on the server
         Notice that a parameter (q) is added to the URL (with the content of the input field)
     
+    The PHP File
+    The page on the server called by the JavaScript above is a PHP file called "livesearch.php".
+
+    The source code in "livesearch.php" searches an XML file for titles matching the search string and returns the result:
+    
+    <?php
+    $xmlDoc=new DOMDocument();
+    $xmlDoc->load("links.xml");
+
+    $x=$xmlDoc->getElementsByTagName('link');
+
+    //get the q parameter from URL
+    $q=$_GET["q"];
+
+    //lookup all links from the xml file if length of q>0
+    if (strlen($q)>0) {
+        $hint="";
+        for($i=0; $i<($x->length); $i++) {
+            $y=$x->item($i)->getElementsByTagName('title');
+            $z=$x->item($i)->getElementsByTagName('url');
+            if ($y->item(0)->nodeType==1) {
+            //find a link matching the search text
+                if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+                        if ($hint=="") {
+                        $hint="<a href='" .
+                        $z->item(0)->childNodes->item(0)->nodeValue .
+                        "' target='_blank'>" .
+                        $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+                        } else {
+                        $hint=$hint . "<br /><a href='" .
+                        $z->item(0)->childNodes->item(0)->nodeValue .
+                        "' target='_blank'>" .
+                        $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+                        }
+                }
+            }
+        }
+    }
+
+    // Set output to "no suggestion" if no hint was found
+    // or to the correct values
+    if ($hint=="") {
+        $response="no suggestion";
+    } else {
+        $response=$hint;
+    }
+
+    //output the response
+    echo $response;
+    ?>
+
+    If there is any text sent from the JavaScript (strlen($q) > 0), the following happens:
+
+        Load an XML file into a new XML DOM object
+        Loop through all <title> elements to find matches from the text sent from the JavaScript
+        Sets the correct url and title in the "$response" variable. If more than one match is found, all matches are added to the variable
+        If no matches are found, the $response variable is set to "no suggestion"
+
     
